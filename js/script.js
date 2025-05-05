3268c0fe4d22dc201f53080c05382efb18bab04e
freerider <freerider.07111@gmail.com>
2025-05-05 18:14:20 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// const result = prompt("are you here?", "");
+// console.log(typeof(result));
+
+const answers = [];
+
+answers[0] = prompt('Имя?', '');
+answers[1] = prompt('Фамилия?', '');
+answers[2] = prompt('Возраст?', '');
+
+document.write(answers);
