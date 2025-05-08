9dd7bbbace44eeb585cd4f2916ae23e76758bd9c
freerider <freerider.07111@gmail.com>
2025-05-08 15:23:34 +0300

    first lesson

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 "use strict";
 
-// const result = prompt("are you here?", "");
-// console.log(typeof(result));
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
-const answers = [];
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-answers[0] = prompt('Имя?', '');
-answers[1] = prompt('Фамилия?', '');
-answers[2] = prompt('Возраст?', '');
+const a = prompt ('Один из последних просмотренных фильмов', ''),
+    b = prompt ('На сколько оцените его', ''),
+    c = prompt ('Один из последних просмотренных фильмов', ''),
+    d = prompt ('На сколько оцените его', '');
 
-document.write(answers);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
